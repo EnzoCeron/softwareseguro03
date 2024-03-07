93bdee3dbff5c75e88b85f35f13a9b6238378718
Enzo Ceron <enzoceron.br@gmail.com>
2024-03-06 22:16:08 -0300

    Tarefa Album Provavelmente Completa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+ 
+        System.out.println("Dados da pessoa-> \n");
+        System.out.println("Digite o nome da pessoa: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite a idade da pessoa: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("--------------------------------");
+        System.out.println("Dados da música-> \n");
+        System.out.println("Digite o título da música: ");
+        String titulo = scanner.nextLine();
+        System.out.println("Digite o compositor da música: ");
+        String compositor = scanner.nextLine();
+        System.out.println("Digite a duração da música: ");
+        float duracao = scanner.nextFloat();
+        scanner.nextLine();
+        System.out.println("--------------------------------");
+        System.out.println("Dados do álbum-> \n");
+        System.out.println("Digite o gênero do álbum: ");
+        String genero = scanner.nextLine();
+        System.out.println("Digite o artista do álbum: ");
+        String artista = scanner.nextLine();
+        System.out.println("Digite o ano do álbum: ");
+        int ano = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o nome do álbum: ");
+        String nomeAlbum = scanner.nextLine();
+
+        System.out.println("Digite a quantidade de músicas do álbum: ");
+        int qtdMusicas = scanner.nextInt();
+        scanner.nextLine();
+        String musicas = "";
+        for (int i = 0; i < qtdMusicas; i++) {
+            System.out.println("Digite o título da música: ");
+            musicas += scanner.nextLine() + ", ";
         }
+        System.out.println("--------------------------------");
+
+        System.out.println("Dados do álbum->");
+        System.out.println("Gênero: " + genero);
+        System.out.println("Artista: " + artista);
+        System.out.println("Ano: " + ano);
+        System.out.println("Nome: " + nomeAlbum);
+        System.out.println("Músicas: " + musicas);
+        System.out.println("--------------------------------");
+
+
+
+     
     }
 }
