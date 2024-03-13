5057ff48cabf9d04bcf83d6430e4d1936d31e4ea
Enzo Ceron <enzoceron.br@gmail.com>
2024-03-13 20:51:48 -0300

    Tarefa 2

---
+++ src/musica.java
@@ -0,0 +1,11 @@
+public class musica {
+    public String nome;
+    public float duracao;
+    public String compositor;
+
+    public musica(String nome, float duracao, String compositor) {
+        this.nome = nome;
+        this.duracao = duracao;
+        this.compositor = compositor;
+    }
+}
